349d9169e5b37b0c00f0242a1d0ba546fe7c0d6f
se62-08 <sopon0714@gmail.com>
2020-03-07 00:59:07 +0700

    บันทึกบัญชีผู้ใช้

--- sign-in-verify.php
+++ sign-in-verify.php
@@ -15,6 +15,13 @@ if ($username == "" || $password == "") {
     } else if ($DATAUSER['status'] == "รอยืนยัน") {
         header("location:./index.php?msg=กรุณายืนยันemail:{$DATAUSER['email']}ก่อน");
     } else {
+        if (isset($_POST['remember'])) {
+            setcookie("username", $username, time() + (10 * 365 * 24 * 60 * 60));
+            setcookie("password", $password, time() + (10 * 365 * 24 * 60 * 60));
+        } else {
+            setcookie("username");
+            setcookie("password");
+        }
         unset($DATAUSER['password']);
         $_SESSION['DATAUSER'] = $DATAUSER;
         header("location:./views/profile/profile.php");
