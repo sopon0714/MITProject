648a25c6ad61662c4575e8dfdaa83226c3c1657d
siripron1601 <pullawongell@hotmail.com>
2020-03-07 14:53:22 +0700

    finish

--- views/config/manage.php
+++ views/config/manage.php
@@ -22,5 +22,46 @@ if (isset($_POST['save'])) {
     WHERE `config`.`config_key` = 'Address'";
     echo $sql . "</br>";
     updateData($sql);
+    // dormitorytel
+    $sql = "UPDATE `config` SET `config_value` = '$dormittel'
+        WHERE `config`.`config_key` = 'dormitorytel'";
+    echo $sql . "</br>";
+    updateData($sql);
+    // email
+    $sql = "UPDATE `config` SET `config_value` = '$email'
+    WHERE `config`.`config_key` = 'email'";
+    echo $sql . "</br>";
+    updateData($sql);
+    // waterbill
+    $sql = "UPDATE `config` SET `config_value` = '$waterbill'
+        WHERE `config`.`config_key` = 'waterbill'";
+    echo $sql . "</br>";
+    updateData($sql);
+    // elecbill
+    $sql = "UPDATE `config` SET `config_value` = '$elecbill'
+    WHERE `config`.`config_key` = 'electricitybill'";
+    echo $sql . "</br>";
+    updateData($sql);
+    // router
+    $sql = "UPDATE `config` SET `config_value` = '$router'
+        WHERE `config`.`config_key` = 'router'";
+    echo $sql . "</br>";
+    updateData($sql);
+    // commonfee
+    $sql = "UPDATE `config` SET `config_value` = '$common'
+    WHERE `config`.`config_key` = 'commonfee'";
+    echo $sql . "</br>";
+    updateData($sql);
+    // account
+    $sql = "UPDATE `config` SET `config_value` = '$account'
+        WHERE `config`.`config_key` = 'account'";
+    echo $sql . "</br>";
+    updateData($sql);
+    // bank
+    $sql = "UPDATE `config` SET `config_value` = '$bank'
+    WHERE `config`.`config_key` = 'bank'";
+    echo $sql . "</br>";
+    updateData($sql);
+
     header("location:../../views/config/config.php");
 }
