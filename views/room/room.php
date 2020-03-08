4a0a23b1e55027a5859bb27079bab0ffe0deb9ad
Khem <kamemachart@hotmail.com>
2020-03-08 23:52:42 +0700

    เสร็จส่วนห้อง

--- views/room/room.php
+++ views/room/room.php
@@ -9,7 +9,7 @@ $DATAUSER = $_SESSION['DATAUSER'] ?? NULL;
     <title>Profile</title>
     <?php require_once('../../views/layout/MainCSS.php');
     include("../../dbConnect.php");
-    $sql_tableRoom = "SELECT room.rid,room.status,rnumber,rent,COALESCE(title,'-') as title,firstname,lastname ,detail 
+    $sql_tableRoom = "SELECT room.rid,room.status as status,rnumber,rent,COALESCE(title,'-') as title,firstname,lastname ,detail ,user.uid
     FROM room LEFT JOIN agreement ON room.rid = agreement.rid LEFT JOIN user ON user.uid = agreement.uid WHERE room.isDelete LIKE 0";
     $sqlnumroom = "SELECT COUNT(rid) AS Numroom FROM room WHERE isDelete LIKE 0";
     $sqlRoomEmpty = "SELECT COUNT(rid) AS Numroom FROM room WHERE isDelete LIKE 0 AND status LIKE 'ว่าง' ";
@@ -108,81 +108,7 @@ $DATAUSER = $_SESSION['DATAUSER'] ?? NULL;
 
                     </div>
                     <!-- ######################## start filter ######################## -->
-                    <div class="row center">
-                        <div class="col-xl-12 col-12 mb-4 ">
-                            <div id="accordion">
-                                <div class="card ">
-                                    <!-- <div class="card-header header-background-color-filter" id="headingOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style=" color:white;">
-											การค้นหาขั้นสูง
-										</div> -->
 
-                                    <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordion">
-                                        <div class="card-body">
-                                            <div class="container">
-                                                <div class="row mb-4">
-                                                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-right">
-                                                        <span>หมายเลขห้อง :</span>
-                                                    </div>
-                                                    <div class="col-lg-6 col-md-8 col-sm-12 col-xs-12">
-                                                        <input type="text" class="form-control" id="username__r" name="username__r" placeholder="กรุณากรอกเลขห้อง">
-                                                    </div>
-                                                </div>
-                                                <div class="row mb-4">
-                                                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-right">
-                                                        <span>ชื่อห้อง/span>
-                                                    </div>
-                                                    <div class="col-lg-6 col-md-8 col-sm-12 col-xs-12">
-                                                        <input type="text" class="form-control" id="username__n_r" name="username__n_r" placeholder="กรุณากรอกชื่อห้อง">
-                                                    </div>
-                                                </div>
-                                                <div class="row mb-4">
-                                                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-right">
-                                                        <span>ชื่อย่อห้อง :</span>
-                                                    </div>
-                                                    <div class="col-lg-6 col-md-8 col-sm-12 col-xs-12">
-                                                        <input type="text" class="form-control" id="username__n_r" name="username__n_r" placeholder="กรุณากรอกชื่อย่อ">
-                                                    </div>
-                                                </div>
-                                                <div class="row mb-4 mt-4">
-                                                    <div class="col-xl-3 col-12 text-right">
-                                                        <span>สถานะห้อง :</span>
-                                                    </div>
-                                                    <div class="col-xl-9 col-12">
-                                                        <div class="form-check form-check-inline">
-                                                            <input type="radio" class="form-check-input" id="status1" name="status">
-                                                            <label class="form-check-label" for="materialInline1">พร้อมใช้งาน</label>
-                                                        </div>
-                                                        <div class="form-check form-check-inline">
-                                                            <input type="radio" class="form-check-input" id="status2" name="status">
-                                                            <label class="form-check-label" for="materialInline2">ไม่พร้อมใช้งาน</label>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="row mb-4 mt-4">
-                                                    <div class="col-xl-3 col-12 text-right">
-                                                        <span>สถานะล็อคห้อง :</span>
-                                                    </div>
-                                                    <div class="col-xl-9 col-12">
-                                                        <div class="form-check form-check-inline">
-                                                            <input type="radio" class="form-check-input" id="status3" name="status2">
-                                                            <label class="form-check-label" for="materialInline1">ล็อค</label>
-                                                        </div>
-                                                        <div class="form-check form-check-inline">
-                                                            <input type="radio" class="form-check-input" id="status4" name="status2">
-                                                            <label class="form-check-label" for="materialInline2">ไม่ล็อค</label>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button class="btn btn-info"><i class="fa fa-search"></i> ค้นหา</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <!-- ######################## end filter ######################## -->
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
@@ -223,19 +149,33 @@ $DATAUSER = $_SESSION['DATAUSER'] ?? NULL;
                                                     <?php for ($i = 0; $i <  $tableRoom[0]['numrow']; $i++) { ?>
                                                         <tr role="row" class="odd">
                                                             <td style="text-align:center;">
-                                                                <a class="btn btn-success btn-square btn-sm active" data-toggle="tooltip" title="" data-original-title="พร้อมใช้งาน">R</a>
+                                                                <?php if ($tableRoom[$i + 1]['status'] == 'ว่าง') { ?>
+                                                                    <a class="btn btn-success btn-square btn-sm active" data-toggle="tooltip" title="" data-original-title="พร้อมใช้งาน">R</a>
+                                                                <?php } else { ?>
+                                                                    <a class="btn btn-danger btn-square btn-sm active" data-toggle="tooltip" title="" data-original-title="ไม่พร้อมใช้งาน">R</a>
+                                                                <?php } ?>
                                                             </td>
                                                             <td><?php echo $tableRoom[$i + 1]['rnumber'] ?></td>
                                                             <td><?php echo $tableRoom[$i + 1]['rent'] ?></td>
                                                             <td><?php echo $tableRoom[$i + 1]['title'] ?> <?php echo $tableRoom[$i + 1]['firstname'] ?> <?php echo $tableRoom[$i + 1]['lastname'] ?> </td>
-                                                            <td><?php echo $tableRoom[$i + 1]['detail'] ?></td>
                                                             <td style="text-align:center;">
-                                                                <a href="#" class="EditRoom" rid=<?php echo  $tableRoom[$i + 1]['rid'] ?> rnumber=<?php echo $tableRoom[$i + 1]['rnumber'] ?> rent=<?php echo $tableRoom[$i + 1]['rent'] ?> detail='<?php echo $tableRoom[$i + 1]['detail'] ?>'>
+                                                                <a href="#" class="detailRoom" detail="<?php echo $tableRoom[$i + 1]['detail']; ?>">
+                                                                    <button type="button" class="btn btn-info btn-sm" data-toggle="tooltip" title='รายละเอียดห้อง'>
+                                                                        <i class="fas fa-file-alt"></i>
+                                                                    </button>
+                                                                </a>
+                                                            </td>
+                                                            <td style="text-align:center;">
+                                                                <a href="#" class="EditRoom" rid=<?php echo  $tableRoom[$i + 1]['rid'] ?> rnumber=<?php echo $tableRoom[$i + 1]['rnumber'] ?> rent=<?php echo $tableRoom[$i + 1]['rent'] ?> detail=' <?php echo $tableRoom[$i + 1]['detail'] ?>'>
                                                                     <button type="button" class="btn btn-warning  btn-sm" 4 data-toggle="tooltip" title="แก้ไขข้อมูล">
                                                                         <i class="fas fa-edit"></i>
                                                                     </button>
                                                                 </a>
-                                                                <button type="button" onclick="delfunction(<?= $tableRoom[$i + 1]['rid'] ?>,<?= $tableRoom[$i + 1]['rnumber'] ?>)" class="btn btn-danger btn-sm btndel" data-toggle="tooltip" title="" data-original-title="ลบห้อง"><i class="far fa-trash-alt"></i></button>
+                                                                <?php if ($tableRoom[$i + 1]['status'] == "ว่าง") { ?>
+                                                                    <button type="button" onclick="delfunction(<?= $tableRoom[$i + 1]['rid'] ?>,<?= $tableRoom[$i + 1]['rnumber'] ?>)" class="btn btn-danger btn-sm btndel" data-toggle="tooltip" title="" data-original-title="ลบห้อง"><i class="far fa-trash-alt"></i></button>
+                                                                <?php } else { ?>
+                                                                    <button type="button" onclick="delfunction2(<?= $tableRoom[$i + 1]['rid'] ?>,<?= $tableRoom[$i + 1]['rnumber'] ?>)" class="btn btn-danger btn-sm btndel" data-toggle="tooltip" title="" data-original-title="ลบห้อง"><i class="far fa-trash-alt"></i></button>
+                                                                <?php } ?>
                                                             </td>
                                                         </tr>
                                                     <?php } ?>
@@ -352,6 +292,35 @@ $DATAUSER = $_SESSION['DATAUSER'] ?? NULL;
         </form>
     </div>
 
+    <div id="modalDetailRoom" class="modal fade">
+        <form class="modal-dialog modal-lg" method="POST" action='manage.php'>
+            <div class="modal-content">
+                <div class="modal-header" style="background-color:#eecc0b">
+                    <h4 class="modal-title" style="color:white">แก้ไขห้อง</h4>
+                </div>
+
+                <div class="modal-body" id="addModalBody">
+                    <div class="row mb-4">
+                        <div class="col-xl-3 col-12 text-right">
+                            <span>รายละเอียด:</span>
+                        </div>
+                        <div class="col-xl-8 col-12">
+                            <!-- <input type="text" class="form-control" id="mail" value="ทีวี ตู้เย็น" placeholder="กรุณากรอกรายละเอียด"> -->
+                            <textarea id="d_detail" name="d_detail" rows="5" cols="60" class="form-control" value="" placeholder=""></textarea>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success">บันทึก</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">ยกเลิก</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+
 
 
 </div>
@@ -397,7 +366,18 @@ $DATAUSER = $_SESSION['DATAUSER'] ?? NULL;
         $("#modalEdit").modal();
     });
 
+    $(".detailRoom").click(function() {
+        var detail = $(this).attr('detail');
+
+        //alert(detail);
+        $('#d_detail').val(detail);
+
+
+        $("#modalDetailRoom").modal();
+    });
+
     function delfunction(id, rname) {
+
         swal({
                 title: "คุณต้องการลบหรือไม่?",
                 text: "ต้องการยืนยันลบห้อง " + rname + " ใช่ไหม ?",
@@ -437,6 +417,21 @@ $DATAUSER = $_SESSION['DATAUSER'] ?? NULL;
                     swal("การลบไม่สำเร็จ กรุณาทำรายการใหม่!");
                 }
             });
+
+
+    }
+
+    function delfunction2(id, rname) {
+
+        swal({
+            title: "คุณไม่สามารถทำรายการได้",
+            text: "คุณไม่สามารถลบได้เนื่องจากห้อง " + rname + " ติดสัญญาอยู่",
+            icon: "error",
+            confirmButtonClass: "btn-danger",
+            dangerMode: true,
+        })
+
+
 
     }
 </script>
